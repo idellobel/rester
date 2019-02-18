20e2237aff96f6139d83054ce73c3364c576111c
idellobel <ivan.dellobel@gmail.com>
2019-02-18 19:55:09 +0100

    Initial commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+import {NgCircleProgressModule} from 'ng-circle-progress';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+     // Specify ng-circle-progress as an import
+     NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+      //3 extra options - zie documentatie onderaan
+      animation: false,
+      responsive: true,
+      renderOnClick: false
+    })
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
